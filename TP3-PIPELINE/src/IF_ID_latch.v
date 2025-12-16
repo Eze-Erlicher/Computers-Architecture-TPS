cb6b93e3e7c8a77f5437419440053806e5d566b3
Juan Badariotti <juan.badariotti@mi.unc.edu.ar>
2025-12-16 20:32:33 -0300

    TP3: added instruction fetch top module

--- TP3-PIPELINE/src/IF_ID_latch.v
+++ TP3-PIPELINE/src/IF_ID_latch.v
@@ -5,19 +5,19 @@ parameter NB_PC = 9
 
 )
 (
-input wire i_clk,
-input wire i_reset,
-input wire i_IF_flush,
-input wire i_IF_ID_write,
-input wire [NB_INSTRUCT-1:0] i_instruction,
-input wire [NB_PC-1:0]i_PC,
+input wire                    i_clk,
+input wire                    i_reset,
+input wire                    i_IF_flush,
+input wire                    i_IF_ID_write,
+input wire [NB_INSTRUCT-1:0]  i_instruction,
+input wire [NB_PC-1:0]        i_PC,
 
-output wire o_instruction,
-output wire o_PC
+output wire [NB_INSTRUCT-1:0] o_instruction,
+output wire [NB_PC-1:0]       o_PC
 );
 
 reg [NB_INSTRUCT-1:0] instruct_buffer;
-reg [NB_PC-1:0] PC_buffer;
+reg [NB_PC-1:0]       PC_buffer;
 
 always @(posedge i_clk)begin
 
