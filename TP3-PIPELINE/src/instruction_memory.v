8b9fdb4a7d361713b96fa286c6c8b8cc7f0088d1
Juan Badariotti <juan.badariotti@mi.unc.edu.ar>
2025-12-26 22:26:09 -0300

    TP3: remove read flag from module and testbench

--- TP3-PIPELINE/src/instruction_memory.v
+++ TP3-PIPELINE/src/instruction_memory.v
@@ -4,19 +4,19 @@ module instruction_memory #(
 
 // memoria  de 512 slots =512*32bits= 2048bytes
     parameter PC_WIDTH = 9, 
-    parameter NB_WIDTH = 32,
-    parameter DEPTH = 2**PC_WIDTH
+    parameter NB_WIDTH = 32
 
 )
 (
     input  wire                i_clk,
     input  wire                i_reset,
-    input  wire                i_read_enable,
     input  wire                i_write_enable,
     input  wire [PC_WIDTH-1:0] i_address,
     input  wire [NB_WIDTH-1:0] write_register,
     output wire [NB_WIDTH-1:0] o_instruction
 );
+
+parameter DEPTH = 2**PC_WIDTH;
 
 reg [NB_WIDTH-1:0] out_instruction;
 reg [NB_WIDTH-1:0] ram_mem [DEPTH-1:0];
@@ -28,14 +28,12 @@ always @(posedge i_clk) begin
         ram_mem[i_address] <= {NB_WIDTH {1'b0}};
     end
 
-    else begin
-        if(i_read_enable) begin
-            out_instruction <= ram_mem[i_address];
-        end
+    else if(i_write_enable) begin
+        ram_mem[i_address] <= write_register;   
+    end
 
-        if(i_write_enable) begin
-            ram_mem[i_address] <= write_register;   
-        end
+    else begin
+        out_instruction <= ram_mem[i_address];
     end
 end
 
